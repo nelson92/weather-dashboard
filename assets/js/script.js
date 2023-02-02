cb46b5ce8cbf17562b2ade305220936bb60322b3
nelson509 <35314794+nelson509@users.noreply.github.com>
2023-02-01 17:24:09 -0800

    added functions to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,47 @@
 var apiKey = "";
 var citySerchList = [];
+
+
+let searchHistory = [];
+
+function localStorage() {
+    return localStorage.getItem('weatherHistory');
+}
+
+function storeSearchHistory() {
+    localStorage.setItem('weatherSearchHistory', newSearchHistory);
+}
+
+function displayWeather(weatherData) {
+    // add code to include weather data on the page
+}
+
+function searchHistoryButtonClick(event) {
+    const cityName = event.target.value;
+    const searchData = searchHistory[cityName];
+    displayWeather({ city: cityName, data: searchData });
+}
+
+async function searchButtonClick() {
+    cityName = $('input').val();
+    // grab city from form
+    // Make API request to get lat/long from cityName
+    // https://openweathermap.org/forecast5#name5
+    // make API request to get 5 day lat long
+    // https://openweathermap.org/forecast5
+    weatherData = await fetch( );
+
+    // save data to local storage
+    searchHistory[cityName] = weatherData;
+    // display new city in search history
+
+    displayWeather({ city: cityName, data: weatherData });
+}
+
+searchHistory = loadSearchHistory();
+Object.keys(searchHistory).forEach((cityName) => {
+    const searchData = searchHistory[cityName];
+    // add code for button for this city
+    $(historyButton).click(searchHistoryButtonClick);
+}
+$('search-btn').click(searchHistoryButton)
