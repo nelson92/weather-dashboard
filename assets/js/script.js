db0d3822cd4318762103b4b805cf07b1386e1338
nelson509 <35314794+nelson509@users.noreply.github.com>
2023-02-01 22:07:25 -0800

    updated functions in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,39 @@
 var apiKey = "bb14c36c10f7318f3cc5c8264cc57b9b";
 // var citySerchList = [];
 
+
 let searchHistory = [];
 
 function localStorage() {
-    return localStorage.getItem('weatherHistory');
+    // return localStorage.getItem('weatherHistory');
+    var searchHistoryArray = JSON.parse(localStorage.getItem('Search History'));
+
+    // if nothing in localStorage, create a new object to track all user info
+    if (!searchHistoryArray) {
+        searchHistoryArray = {
+            searchedCity: [],
+        };
+    } else {
+        //add search history buttons to page
+        for (var i = 0; i < searchHistoryArray.searchedCity.length; i++) {
+            searchHistory(searchHistoryArray.searchedCity[i]);
+        }
+    }
+
+    return searchHistoryArray;
 }
 
 function storeSearchHistory() {
-    localStorage.setItem('weatherSearchHistory', newSearchHistory);
+    localStorage.setItem('weather Search History', JSON.stringify(searchHistoryArray));
 }
 
 function displayWeather(weatherData) {
     // add code to include weather data on the page
+    event.preventDefault();
+    if (searchCity.val().trim() !== "") {
+        city = serchCity.val().trim();
+        currentWeather(city);
+    }
 }
 
 function searchHistoryButtonClick(event) {
